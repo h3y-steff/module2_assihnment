b1072026263bef6e833f610313b737696c45b902
h3y-steff <156868335+h3y-steff@users.noreply.github.com>
2025-01-31 18:27:31 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+//the following is the old way of creating variables //
+
+//var life=100; //this is my life bar//
+
+//life=life-10; //life will now show up as 90//
+
+
+//var name="Dev Ed"; //this is the name displayed in the game STRING//
+
+//var checkout=false; //this is a boolean//
+
+//var box; //this will show up in your console box as undefined//
+
+//var box=null;
+
+//console.log(box)
+
+// the following is the new way of making variables//
+
+//const life=100; //const means the data connot be modified//
+
+let life=100;  //let means you can modify the data//
+
+life=life-50
+
+
+console.log(life)
